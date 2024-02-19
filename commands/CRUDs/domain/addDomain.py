7abd4e55631d3a41077612e950c67fb17c8c0113
XIB <bahaeddine.ksib@esprit.tn>
2024-02-19 08:48:02 -0500

    add domain - complete

--- commands/CRUDs/domain/addDomain.py
+++ commands/CRUDs/domain/addDomain.py
@@ -7,24 +7,60 @@ import GlobalVars as TopG
 class AddDomain:
 	@staticmethod
 	def execute(IN):
-		IN       = c.short_command(IN,"ad")
-		ad       = c.option("ad",    False,False,IN)
-		domain   = c.option("-d",    True, False,IN)
-		w_id     = c.option("-w",    True, False,IN)
-		tags     = c.option("--tag", True, True, IN)
-		for_sure = c.option("-s",    False,False, IN)
+		IN         = c.short_command(IN,"ad")
+		ad         = c.option("ad",           False,False,IN)
+		domain     = c.option("-d",           True, False,IN)
+		w_id       = c.option("-w",           True, False,IN)
+		tags       = c.option("--tag",        True, True, IN)
+		techs      = c.option("--tech",       True, True, IN)
+		whois_file = c.option("--whois-file", True, False,IN)
+		ip         = c.option("--ip",         True, False,IN)
+		ports_map  = c.option("--port",       True, True, IN)
+		server_file= c.option("--server-file",True, False,IN)
+		robots_file= c.option("--robots-file",True, False,IN)
+		js_files   = c.option("--js-file",    True, True, IN)
+		for_sure   = c.option("-s",           False,False,IN)
 		
 
 
-		if("UserNeedsHelp" in [ad, domain, for_sure, tags] or (not domain)):
+		if("UserNeedsHelp" in [ ad,
+					domain,
+					for_sure,
+					tags,
+					techs,
+					whois_file,
+					ip,
+					ports_map,
+					server_file,
+					robots_file,
+					js_files] or (not domain)):
 			AddDomain.help()
 		elif(not w_id and TopG.CURRENT_WORKSHOP == ""):
 			print("❌ Set a Workshop or specify a workshop with [-w <workshop id>]")
-
+		elif(ports_map and not c.canBeMap(ports_map)):
+			print("❌Ports format: <[PORT_NAME]:[PORT]>")
 		else:
-			w_id = TopG.CURRENT_WORKSHOP if not w_id else w_id
-			tags  = [] if not tags else tags
-			dmn = Domain(workshop_id=w_id, domain_text=domain, tags=tags)
+			cw         = TopG.CURRENT_WORKSHOP
+			w_id       = cw if not w_id        else w_id
+			tags       = [] if not tags        else tags
+			techs      = [] if not techs       else techs
+			whois_file = "" if not whois_file  else whois_file
+			ip         = "" if not ip          else ip
+			ports_map  = {} if not ports_map   else c.listToMap(ports_map)
+			server_file= "" if not server_file else server_file
+			robots_file= "" if not robots_file else robots_file
+			js_files   = [] if not js_files    else js_files
+
+			dmn   = Domain( workshop_id     =w_id,
+					domain_text     =domain,
+					tags            =tags,
+					techs_list      =techs,
+					whois_file      =whois_file,
+					ip              =ip,
+					ports_map       =ports_map,
+					server_file     =server_file,
+					robots_txt_file =robots_file,
+					js_files_list   = js_files)
 			print("\ndomain:")
 			dmn.display()
 			result = c.questionToExecute(for_sure,dmn.save,{},"Save domain ["+domain+"] ?")
@@ -40,3 +76,10 @@ class AddDomain:
 		print("help -AddDomain")
 
 
+
+
+
+
+
+#TESTING COMMAND
+#ad -d www.disc.study -s --tag in-soup focus fromManual --tech react java --whois-file /whois/discord --ip 192.168.0.1 --port https:448 ssh:25 --server-file /nmap/scan --robots-file /disc/robots.txt --js-file /js1 /js2 /js3
--- commands/DRYFFC.py
+++ commands/DRYFFC.py
@@ -1,4 +1,4 @@
-
+import re
 
 
 # DRYFFC : DON'T REPEAT YOURSELF FUNCTIONS FOR COMMANDS
@@ -16,7 +16,6 @@ def option(op,value,MultiValues,IN):
 					valList=[]
 					count=1
 					while(IN.split().index(op)+count < len(IN.split()) and IN.split()[IN.split().index(op)+count][0]!= '-'):
-						print("DEBUG count:"+str(count))
 						val=IN.split()[IN.split().index(op)+count]
 						valList.append(val)
 						count += 1
@@ -60,5 +59,17 @@ def questionToExecute(for_sure, func, arguments, question):
 			ask = False if y_n in ['yes','no','y','n'] else True
 		if(y_n == 'y'):
 			return func(**arguments)
+def canBeMap(LP):
+	for l in LP:
+		if not re.match(r'^\w+:\d+$', l):
+			return False
+	return True
+
+
+def listToMap(LP):
+	Map={}
+	for l in LP:
+		Map[l.split(':')[0]] = int(l.split(':')[1])
+	return Map
 
 
